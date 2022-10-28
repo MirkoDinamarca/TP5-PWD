b17238a6d1c49115af74bc4c17315ef5cc588f06
Agustina Kilapi Viecens <70908771+agustinakilapi52@users.noreply.github.com>
2022-10-28 16:48:24 -0300

    metodo insertar buscar y eliminar

--- Model/Model_usuariorol.php
+++ Model/Model_usuariorol.php
@@ -34,6 +34,86 @@ class Model_usuariorol extends BaseDatos {
     }
 
     // Hay que hacer los métodos Insertar, Modificar, Eliminar, Buscar y Listar?
+     /**
+     * Inserta un nuevo usuario usuariorol a la BD
+     * (Aún no se hicieron inserciones en la BD, no sabemos si funciona)
+     */
+
+    public function Insertar() {
+        $rta = false;
+
+        $query = "INSERT INTO usuariorol(idusuario, idrol)
+                  VALUES ('" . $this->getObjUsuario()->getidusuario() . "','" . $this->getObjRol()->getIdRol(). "')";
+
+        if ($this->Iniciar()) {
+            if ($this->Ejecutar($query)) {
+                $rta = true;
+            } else {
+                $this->getError();
+            }
+        } else {
+            $this->getError();
+        }
+
+        return $rta;
+    }
+  
+
+       /**
+     * Método para eliminar un usuariorol de la BD
+     * (Aún no se eliminaron datos en la BD, no sabemos si funciona)
+     */
+
+    public function Eliminar() {
+        $rta = false;
+
+        $query = "DELETE FROM usuariorol WHERE idusuario=" . $this->getObjUsuario()->getidusuario();
+
+        if ($this->Iniciar()) {
+            if ($this->Ejecutar($query)) {
+                $rta = true;
+            } else {
+                $this->getError();
+            }
+        } else {
+            $this->getError();
+        }
+
+        return $rta;
+    }
+
+        /**
+     * Recupera los datos de un usuariorol a través de su $idusuario
+     * (Probar si funciona)
+     * @param idusuario int
+     */
+    public function Buscar($idusuario) {
+
+        $query = "SELECT * FROM usuariorol WHERE idusuario =" .$idusuario;
+
+        $rta = false;
+
+        if ($this->Iniciar()) {
+            if ($this->Ejecutar($query)) {
+                if ($row = $this->Registro()) {
+                    $objUsuario = new Model_usuario();
+                    $objUsuario->Buscar($row['idusuario']);
+                    $objRol = new Model_rol();
+                    $objRol->Buscar($row['idrol']);
+
+                    $this->setearValores($objUsuario,$objRol);
+
+                    $rta = true;
+                }
+            } else {
+                $this->setMsjOperacion($this->getError());
+            }
+        } else {
+            $this->setMsjOperacion($this->getError());
+        }
+        return $rta;
+    }
+
 }
 
 
